0528923891d935963cde4cb2d157e51e046e83b1
Jakub Pomorski <delveller1@gmail.com>
2023-06-05 13:09:41 +0200

    redesign

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,26 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className="h-100vh flex text-white bg-green-900 items-center justify-center">
-            <div>
-                <h1 className="font-semibold px-3 py-3 text-3xl text-center border-2 border-white rounded">
-                    Wybierz jedną z opcji:
-                </h1>
-                <div className="text-black text-3xl flex items-center justify-center">
-                    <div className="px-4 py-4">
-                        <Link className="bg-white w-64 py-3 block text-center rounded cursor-pointer" to="/sprawnosci">
-                            Sprawności
-                        </Link>
-                    </div>
-                    <div className="px-4 py-4">
-                        <Link className="bg-white w-64 py-3 block text-center rounded cursor-pointer" to="/Stopien">
-                            Stopień
-                        </Link>
+        <div>
+            <h1 className="py-2 px-2 text-white font-semibold text-2xl text-border-green bot-border">
+                40 KDH Barykada
+            </h1>
+            <div className="h-100vh flex text-white bg-green-900 items-center justify-center">
+                <div>
+                    <h2 className="font-semibold px-3 py-3 text-3xl text-center border-2 border-white rounded">
+                        Wybierz jedną z opcji:
+                    </h2>
+                    <div className="text-black text-3xl flex items-center justify-center">
+                        <div className="px-4 py-4">
+                            <Link className="bg-white w-64 py-3 block border-black text-center rounded cursor-pointer" to="/sprawnosci">
+                                Sprawności
+                            </Link>
+                        </div>
+                        <div className="px-4 py-4">
+                            <Link className="bg-white w-64 py-3 block border-black text-center rounded cursor-pointer" to="/Stopien">
+                                Stopień
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/stpn.js
+++ src/stpn.js
@@ -14,8 +14,8 @@ const Stopien = () => {
     React.useEffect(() => {
         const URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSP8edhewJtAiYJ4E3Mc2DSq_3YGIU9uT1KzfTuCQi87l6Qv6b5VmchkJkR2KC0mapIFGmBdmLYskm6/pub?gid=0&single=true&output=csv";
         fetch(URL)
-            .then(response => response.json())
             .then(data => {
+                console.log(data)
                 setRows(data.split("\n"));
             });
     }, []);
